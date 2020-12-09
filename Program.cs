a8aee9bf41a75344aa579eb348a2b2a6089169cf
Rodrigo Viana <rviana97@gmail.com>
2020-12-09 19:15:32 -0300

    Add function to read card values from console input

--- Program.cs
+++ Program.cs
@@ -4,9 +4,38 @@ namespace Cards
 {
     class Program
     {
+        const int cardCount = 5;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] cards = ReadConsoleInput();
+        }
+
+        private static int[] ReadConsoleInput()
+        {
+            int[] ret = new int[cardCount];
+            bool isInputValid = false;
+            while (!isInputValid)
+            {
+                Console.WriteLine("Please input 5 space-separated integers");
+                string input = Console.ReadLine();
+                string[] cardStrings = input.Split(" ");
+
+                if (cardStrings.Length == 5)
+                {
+                    isInputValid = true;
+                    for (int i = 0; i < cardCount; i++)
+                    {
+                        if (!int.TryParse(cardStrings[i], out ret[i]))
+                        {
+                            isInputValid = false;
+
+                            break;
+                        }
+                    }
+                }
+            }
+            return ret;
         }
     }
 }
